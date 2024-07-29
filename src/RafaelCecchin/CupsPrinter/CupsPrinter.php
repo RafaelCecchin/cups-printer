9a590268b74c44191b7d1b0a426b7c8c77d4e5ab
Rafael Cecchin <contatorafaelcecchin@gmail.com>
2024-07-29 09:30:12 -0300

    feat: adicionar projeto

---
+++ src/RafaelCecchin/CupsPrinter/CupsPrinter.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace RafaelCecchin\CupsPrinter;
+
+class CupsPrinter
+{
+    public $printerServer;
+    public $printerName;
+
+    function __construct(String $printerServer, String $printerName) 
+    {
+        $this->printerServer = $printerServer;
+        $this->printerName = $printerName;
+    }
+
+    public function printData(String $data) 
+    {
+        $tempFile = $this->createTempFile($data);
+        $requestId = $this->printFile($tempFile);
+        $this->removeTempFile($tempFile);
+
+        return $requestId;
+    }
+
+    public function printFile($fileLocation)
+    {
+        $cmdPrintCommand = $this->cmdPrintCommand($fileLocation);
+        $cmdOutput = $this->execPrint($cmdPrintCommand);
+        $requestId = $this->extractRequestId($cmdOutput);
+
+        return $requestId;
+    }
+
+    public function cmdPrintCommand(String $tempFile)
+    {
+        $cmd = "lp -h $this->printerServer -d $this->printerName $tempFile";
+
+        return $cmd;
+    }
+
+    private function execPrint(String $cmdPrintCommand) 
+    {
+        exec($cmdPrintCommand, $cmdOutput, $returnVar);
+
+        return $cmdOutput;
+    }
+
+    private function createTempFile(String $data, String $prefix =  'print_')
+    {
+        $tempDir = sys_get_temp_dir();
+        $tempFile = tempnam($tempDir, $prefix);
+        $file = fopen($tempFile, 'w');
+        fwrite($file, $data);
+        fclose($file);
+
+        return $tempFile;
+    }
+
+    private function removeTempFile(String $tempFile)
+    {
+        unlink($tempFile);
+    }
+
+    private function extractRequestId($cmdOutputArray) 
+    {
+        $pattern = '/request id is ([A-Za-z0-9\-]+)/';
+
+        if (empty($cmdOutputArray)) {
+            return null;
+        }
+
+        $message = $cmdOutputArray[0];
+        $match = preg_match($pattern, $message, $matches);
+
+        if (!$match) {
+            return null;
+        }
+
+        return $matches[1];
+    }
+}
